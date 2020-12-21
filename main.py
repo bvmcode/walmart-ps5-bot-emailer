47eee822fa745b7e164391a106128cbb03af4396
Barry Martin <MARTBAR@chrobinson.com>
2020-12-21 08:50:56 -0500

    change structure

--- main.py
+++ main.py
@@ -33,43 +33,40 @@ def email(name, url):
     try:
         with yagmail.SMTP(user, app_password) as yag:
             yag.send(to, subject, content)
-        logging.info(f'{urls[key]} - email success')
+        logging.info(f'{key} - email success')
     except:
-        logging.critical(f'{urls[key]} - email failed')
+        logging.critical(f'{key} - email failed')
 
 
 
 def check_walmart():
     """ 
-         check if consoles are in stock at walmart
-         'This item is out of stock.' message found in class 'prod-blitz-copy-message'
+         check if the button text 'Add to cart' exists
     """
-
+    logging.info('Start bot')
+    
     urls = {
         'PS5 Digital Edition': 'https://www.walmart.com/ip/Sony-PlayStation-5-Digital-Edition/493824815',
         'PS5 Console': 'https://www.walmart.com/ip/PlayStation-5-Console/363472942'
     }
-    
 
     driver = webdriver.Chrome()
-
-    logging.info('Start bot')
+    class_name = 'prod-product-cta-add-to-cart'
 
     for key in urls:
         driver.get(urls[key])
         logging.info(f'{urls[key]} - downloaded')
         try:
-            element_present = EC.presence_of_element_located((By.CLASS_NAME, 'prod-blitz-copy-message'))
-            WebDriverWait(driver, 10).until(element_present)
-            item_message = driver.find_element_by_class_name('prod-blitz-copy-message').text
-            if 'This item is out of stock.' in item_message:
-                logging.info(f'{urls[key]} - render succeeded with class "prod-blitz-copy-message" - item not available')
+            element_present = EC.presence_of_element_located((By.CLASS_NAME, class_name))
+            WebDriverWait(driver, 5).until(element_present)
+            item_message = driver.find_element_by_class_name(class_name).text
+            if 'Add to cart' in item_message:
+                logging.info(f'{key} - In Stock')
+                email(key, urls[key])
         except TimeoutException:
-            logging.info(f'{urls[key]} - render time out with class "prod-blitz-copy-message" - item available')
-            email(key, urls[key])
-    
-    
+            logging.info(f'{key} - Not in stock')    
     driver.quit()
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, filename="./logs/main.log", format='%(asctime)s:%(levelname)s:%(message)s')
